934d22c36b62db1250e4aa8c7b157a1533761447
Ryan <leeyuenhigh@gmail.com>
2021-03-06 11:33:00 -0500

    Completed drill

---
+++ HashMaps_drills.js
@@ -0,0 +1,26 @@
+const HashMap = require('./HashMap')
+
+function main(){
+  let lotr = new HashMap(4)
+  lotr.MAX_LOAD_RATIO = 0.5
+  lotr.SIZE_RATIO = 3
+  lotr.set("Hobbit","Bilbo")
+  lotr.set('Hobbit','Frodo')
+  lotr.set('Wizard','Gandalf')
+  lotr.set('Human','Aragorn')
+  lotr.set('Elf','Legolas')
+  lotr.set('Maiar','The Necromancer')
+  lotr.set('Maiar','Sauron')
+  lotr.set('RingBearer',"Gollum")
+  lotr.set('LadyOfLight',"Galadriel")
+  lotr.set('HalfElven','Arwen')
+  lotr.set('Ent','TreeBeard')
+  console.log(lotr.get('Maiar'))
+  console.log(lotr.get('Hobbit'))
+  return lotr
+}
+
+console.log(main())
+
+// There isn't a discrepancy since you are just replacing the orignal values for Hobbit
+// and Maiar when you do a 2nd call using the same key
